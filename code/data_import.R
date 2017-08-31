60d6cb1bfda15b8cdbac8248f45b541cfe9d5452
lmwang9527 <lmwang@gmail.com>
2017-08-31 13:03:23 -0700

    live coding during class

--- code/data_import.R
+++ code/data_import.R
@@ -7,3 +7,29 @@ test_df <- read_fwf("data/NCDC-CDO-USC00356750.txt",
                                                   end=c(19, 69, 120),
                                                   col_names = c("STATION", "NAME", "PRCP")),
                     skip=2)
+
+library(tidyverse)
+require(readxl)
+
+(tilikum <- read_excel("data/Tilikum Crossing daily bike counts 2015-16 082117.xlsx", skip=1))
+hawthorne <- read_excel("data/Hawthorne Bridge daily bike counts 2012-2016 082117.xlsx")
+
+tilikum$bridge <- "Tilikum"
+hawthorne$bridge <- "Hawthorne"
+
+names(hawthorne) <- c("date", "westbound", "eastbound", "total", "bridge")
+
+bikecounts <- bind_rows(tilikum, hawthorne)
+
+bikecounts <- bikecounts %>% 
+  gather(westbound, eastbound, key="direction", value="counts")
+
+bikecounts %>% 
+  gather(westbound, eastbound, key="direction", value="counts") -> bikecounts
+
+bikecounts %>% 
+  mutate(week=week(date),
+         month=month(date),
+         year=year(date))
+
+nhts <- read_csv("data/NHTS2009_dd.csv")
--- code/tidy_data.R
+++ code/tidy_data.R
@@ -9,4 +9,16 @@ table6 <- tribble(
   3,          "bike, transit",    "6,15"
 )
 
-#separate_rows(table6, modes, time, convert = TRUE)
+separate_rows(table6, modes, time, convert = TRUE)
+
+table4 <- table4a %>% 
+  left_join(table4b, by="country", 
+            suffix=c("_cases", "_population"))
+
+gather(table4, 
+       `1999_cases`:`2000_population`, 
+       key='year_type', value="counts") %>% 
+  separate(year_type, into=c("year", "type")) %>% 
+  spread(key=type, value=counts)
+
+
