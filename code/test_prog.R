4d13a5efdad7a5962d5a9a2ceffbd4d5d1fe3bfe
lmwang <lmwang@gmail.com>
2018-08-05 17:15:20 -0700

    committed changes made in the 2017 iteration

--- code/test_prog.R
+++ code/test_prog.R
@@ -1,4 +1,8 @@
 require(rlang)
+require(readr)
+require(dplyr)
+require(tidyr)
+
 trip_rates <- function(dd, var1, var2) {
   var1_quo <- enquo(var1)
   var2_quo <- enquo(var2)
@@ -7,10 +11,14 @@ trip_rates <- function(dd, var1, var2) {
     dplyr::summarize(var1=first(!!var1_quo), 
                      var2=first(!!var2_quo), 
                      ntrips=n())
+  x <- "var1"
+  x2 <- paste0(var1_quo)[2]
+  print(x2)
   hh %>% 
     group_by(var1, var2) %>% 
     summarize(avg_trip=mean(ntrips)) %>% 
-    spread(key=var2, value=avg_trip) #%>% 
+    spread(key=var2, value=avg_trip) %>% 
+    rename(!!x2 := var1)
     #select(var1, `low income`, `med income`, `high income`)
 }
 
